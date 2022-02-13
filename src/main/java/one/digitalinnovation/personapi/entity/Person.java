ad340a884c7728d3c343bad34a40f488efee861b
Cuckoo-Head <cuckoohead@disroot.org>
2022-02-13 17:39:54 -0300

    added attributes

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -2,6 +2,8 @@ package one.digitalinnovation.personapi.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
@@ -11,4 +13,15 @@ import lombok.Builder;
 @NoArgsConstructor
 public class Person {
 
+	private Long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String cpf;
+	
+	private LocalDate birthDate;
+	
+	private List<Phone> phones;
 }
