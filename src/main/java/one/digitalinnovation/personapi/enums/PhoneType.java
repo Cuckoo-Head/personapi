6c98def7f5f4a68dcd61dbbefb4cbd7d62f96705
Cuckoo-Head <cuckoohead@disroot.org>
2022-02-13 17:29:58 -0300

    fix PhoneType as enum, not class, created PhoneType types and Phone class is ready

--- src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
+++ src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
@@ -1,5 +1,13 @@
 package one.digitalinnovation.personapi.enums;
 
-public class PhoneType {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+	
+	HOME,
+	MOBILE,
+	COMMERCIAL;
 }
