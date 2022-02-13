f51eca1b40d054b723b5f5b4c274d9185b1a25e5
Cuckoo-Head <cuckoohead@disroot.org>
2022-02-13 17:36:27 -0300

    imported and added Lombok annotations

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -1,5 +1,14 @@
 package one.digitalinnovation.personapi.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
 
 }
