ce1877c9e85755e36c2249bc7fa99b3edc007d9c
Cuckoo-Head <cuckoohead@disroot.org>
2022-02-14 16:49:54 -0300

    creation of postPerson method

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,12 +1,14 @@
 package one.digitalinnovation.personapi.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 
 @RestController
@@ -21,7 +23,11 @@ public class PersonController {
 	}
 	
 	@PostMapping
-	public String postPerson() {
-		return "API Test!";
+	public MessageResponseDTO postPerson(@RequestBody Person person) {
+		Person savedPerson = personRepository.save(person);
+		return MessageResponseDTO
+				.builder()
+				.message("Create person with ID: " + savedPerson.getId())
+				.build();
 	}
 }
