019df6f9549e746d38e090968364212808f3cfce
Cuckoo-Head <cuckoohead@disroot.org>
2022-02-14 15:54:52 -0300

    PersonRepository dependency injection

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,14 +1,25 @@
 package one.digitalinnovation.personapi.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalinnovation.personapi.repository.PersonRepository;
+
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
+	private PersonRepository personRepository;
+	
+	@Autowired
+	public PersonController(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+	
 	@PostMapping
 	public String postPerson() {
 		return "API Test!";
